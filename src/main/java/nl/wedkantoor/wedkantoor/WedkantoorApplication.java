0e5f0e8ce97e5c826f7340eb4f96b1876d2df48b
Jennes <jennesderonde@msn.com>
2018-08-11 11:36:29 +0200

    first commit

---
+++ src/main/java/nl/wedkantoor/wedkantoor/WedkantoorApplication.java
@@ -0,0 +1,12 @@
+package nl.wedkantoor.wedkantoor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedkantoorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WedkantoorApplication.class, args);
+	}
+}
